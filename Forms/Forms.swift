f03ca87fc6fcc0106f0d83f0e4405c027f8108c3
Chris Eidhof <chris@eidhof.nl>
2018-03-14 08:15:52 +0100

    Checkpoint

--- Forms/Forms.swift
+++ Forms/Forms.swift
@@ -13,6 +13,8 @@ public protocol Element {
     associatedtype Message
     static func label(text: @escaping (Input) -> String) -> Self
     static func button(title: @escaping (Input) -> String, enabled: @escaping (Input) -> Bool, onTap: Message) -> Self
+    static func `switch`(isOn: WritableKeyPath<Input, Bool>) -> Self
+
     static func _textField(text: WritableKeyPath<Input, String>, isSecure: @escaping (Input) -> Bool, backgroundColor: @escaping (Input) -> UIColor) -> Self
 }
 
@@ -27,11 +29,11 @@ extension Element {
 
 }
 
-public struct RenderedElement<I> {
-    let view: UIView
+public struct RenderedElement<I, V> {
+    let view: V
     let strongReferences: [Any]
     let inputChanged: (I) -> ()
-    init(view: UIView, inputChanged: @escaping (I) -> (), strongReferences: [Any] = []) {
+    init(view: V, inputChanged: @escaping (I) -> (), strongReferences: [Any] = []) {
         self.view = view
         self.inputChanged = inputChanged
         self.strongReferences = strongReferences
@@ -45,7 +47,7 @@ enum Change<Input, Message> {
 }
 
 public struct Renderer<I, M> {
-    let render: (_ change: @escaping (Change<I, M>) -> ()) -> RenderedElement<I>
+    let render: (_ change: @escaping (Change<I, M>) -> (), _ align: NSTextAlignment) -> RenderedElement<I, UIView>
 }
 
 private final class TargetAction {
@@ -64,8 +66,9 @@ extension Renderer: Element {
     public typealias Message = M
     
     public static func label(text: @escaping (Input) -> String) -> Renderer<Input, Message> {
-        return Renderer { change in
+        return Renderer { change, alignment in
             let l = UILabel()
+            l.textAlignment = alignment
             return RenderedElement(view: l, inputChanged: {
                     l.text = text($0)
             })
@@ -73,8 +76,9 @@ extension Renderer: Element {
     }
     
     public static func _textField(text: WritableKeyPath<Input, String>, isSecure: @escaping (Input) -> Bool, backgroundColor: @escaping (Input) -> UIColor) -> Renderer<Input, Message> {
-        return Renderer { change in
+        return Renderer { change, alignment in
             let t = UITextField()
+            t.textAlignment = alignment
             let ta = TargetAction {
                 change(.stateChange { (s: inout Input) in
                     s[keyPath: text] = t.text ?? ""
@@ -91,8 +95,25 @@ extension Renderer: Element {
         }
     }
     
+    public static func `switch`(isOn: WritableKeyPath<Input,  Bool>) -> Renderer<Input, Message> {
+        return Renderer { change, alignment in
+            let t = UISwitch()
+            let ta = TargetAction {
+                change(.stateChange { (s: inout Input) in
+                    s[keyPath: isOn] = t.isOn
+                    })
+            }
+            t.addTarget(ta, action: #selector(TargetAction.action(sender:)), for: .valueChanged)
+            return RenderedElement(view: t, inputChanged: { i in
+                if t.isOn != i[keyPath: isOn] {
+                    t.isOn = i[keyPath: isOn]
+                }
+            }, strongReferences: [ta])
+        }
+    }
+    
     public static func button(title: @escaping (Input) -> String, enabled: @escaping (Input) -> Bool, onTap: Message) -> Renderer<Input, Message> {
-        return Renderer { change in
+        return Renderer { change, alignment in
             let ta = TargetAction {
                 change(.message(onTap))
             }
@@ -115,8 +136,113 @@ public func stackView<State, Message>(initial: State, renderers: [Renderer<State
             updateForChangedState()
         }
     }
-    let elements = renderers.map { (r: Renderer<State, Message>) -> RenderedElement<State> in
+    let elements = renderers.map { (r: Renderer<State, Message>) -> RenderedElement<State, UIView> in
         r.render( { change in
+            switch change {
+            case let .message(m):
+                message(m)
+            case let .stateChange(f):
+                f(&state)
+            }
+        }, .left)
+    }
+    let refs = elements.map { $0.strongReferences }
+    updateForChangedState = { elements.forEach { $0.inputChanged(state) } }
+    updateForChangedState()
+    return (UIStackView(arrangedSubviews: elements.map { $0.view }), refs)
+}
+
+extension UIView {
+    var frameRight: CGFloat? {
+        get {
+            guard let s = superview else { return nil }
+            return s.bounds.width - frame.maxX
+        }
+        set {
+            guard let s = superview else { return }
+            frame.origin.x = s.bounds.width - frame.width - (newValue ?? 0)
+            print(s.bounds.width, frame.width, (newValue ?? 0))
+        }
+    }
+    
+    func centerVertically() {
+        guard let s = superview else { return }
+        frame.origin.y = (s.bounds.height - frame.height) / 2
+    }
+}
+
+final class FormCell: UITableViewCell {
+    let formElement: UIView
+    
+    init(label: String, _ formElement: UIView) {
+        self.formElement = formElement
+        super.init(style: .value1, reuseIdentifier: nil)
+        textLabel?.text = label
+        contentView.addSubview(formElement)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        formElement.frameRight = 20
+        formElement.centerVertically()
+    }
+}
+
+final class StaticTableViewConfig: NSObject, UITableViewDataSource {
+    var cells: [UITableViewCell]
+    let tableView: UITableView
+    init(tableView: UITableView, cells: [UITableViewCell]) {
+        self.tableView = tableView
+        self.cells = cells
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cells.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return cells[indexPath.row]
+    }
+}
+
+public struct Cell<State, Message> {
+    let text: (State) -> String
+    let backgroundColor: (State) -> UIColor
+    let element: Renderer<State, Message>
+    public init(_ text: @escaping (State) -> String, _ element: Renderer<State, Message>, backgroundColor: @escaping (State) -> UIColor = { _ in .white }) {
+        self.text = text
+        self.element = element
+        self.backgroundColor = backgroundColor
+    }
+}
+
+extension Cell {
+    func render(_ change: @escaping (Change<State, Message>) -> ()) -> RenderedElement<State, UITableViewCell> {
+        let re = element.render(change, .right)
+        re.view.frame.size = CGSize(width: 200, height: 30) // todo
+        let cell = FormCell(label: "one", re.view)
+        return RenderedElement<State, UITableViewCell>(view: cell, inputChanged: { newInput in
+            cell.textLabel?.text = self.text(newInput)
+            cell.backgroundColor = self.backgroundColor(newInput)
+            re.inputChanged(newInput)
+        }, strongReferences: re.strongReferences)
+    }
+}
+
+public func tableView<State, Message>(initial: State, cells: [Cell<State, Message>], message: @escaping (Message) -> ()) -> (tableView: UITableView, strongReferences: [Any]) {
+    var updateForChangedState: () -> () = {}
+    var state = initial {
+        didSet {
+            updateForChangedState()
+            print(state)
+        }
+    }
+    let elements = cells.map { (c: Cell<State, Message>) -> RenderedElement<State, UITableViewCell> in
+        return c.render( { change in
             switch change {
             case let .message(m):
                 message(m)
@@ -125,8 +251,14 @@ public func stackView<State, Message>(initial: State, renderers: [Renderer<State
             }
         })
     }
-    let refs = elements.map { $0.strongReferences }
+    var refs = elements.map { $0.strongReferences }
     updateForChangedState = { elements.forEach { $0.inputChanged(state) } }
     updateForChangedState()
-    return (UIStackView(arrangedSubviews: elements.map { $0.view }), refs)
+    let result = UITableView(frame: .zero, style: .grouped)
+    let manager = StaticTableViewConfig(tableView: result, cells: elements.map { el in
+        return el.view
+    })
+    result.dataSource = manager
+    refs.append([manager])
+    return (result, refs)
 }
--- FormsExample/ViewController.swift
+++ FormsExample/ViewController.swift
@@ -12,6 +12,7 @@ import Forms
 struct State {
     var name: String = "Your Name"
     var password: String = "The Password"
+    var other: Bool = false
     
     var valid: Bool {
         return !name.isEmpty && !password.isEmpty
@@ -25,20 +26,53 @@ struct State {
     }
 }
 
+func form<E: Element>() -> [E] where E.Input == State, E.Message == State.Message {
+    return [
+        .label { _ in "Name" },
+        .textField(text: \.name, backgroundColor: { $0.validName ? .white : .red }),
+        .label { _ in "Password" },
+        .textField(text: \.password, isSecure: { _ in true }, backgroundColor: { $0.validPassword ? .white : .red }),
+        .button(title: { _ in "Submit" }, enabled: { $0.valid }, onTap: State.Message.submit)
+    ]
+}
+
+func tableForm() -> [Cell<State,State.Message>] {
+    return [
+    Cell({ _ in "Name" }, .textField(text: \.name, backgroundColor: { $0.validName ? .white : .red })),
+    Cell({ _ in "Password" }, .textField(text: \.password, isSecure: { _ in true }, backgroundColor: { $0.validPassword ? .white : .red }), backgroundColor: { $0.validPassword ? .white : .red }),
+    Cell({ _ in "Spam" }, .switch(isOn: \.other))
+    ]
+}
+
 class ViewController: UIViewController {
     var refs: [Any] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let (s, refs) = stackView(initial: State(), renderers: [
-            .label { _ in "Name" },
-            .textField(text: \.name, backgroundColor: { $0.validName ? .white : .red }),
-            .label { _ in "Password" },
-            .textField(text: \.password, isSecure: { _ in true }, backgroundColor: { $0.validPassword ? .white : .red }),
-            .button(title: { _ in "Submit" }, enabled: { $0.valid }, onTap: State.Message.submit)
-            ], message: {
-                print($0)
+        buildTableView()
+    }
+    
+    func buildTableView() {
+        let (s, refs) = tableView(initial: State(), cells: tableForm(), message: {
+            print($0)
+        })
+        self.refs = refs
+        
+        s.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(s)
+        let si = view.safeAreaLayoutGuide
+        view.addConstraints([
+            s.topAnchor.constraint(equalTo: si.topAnchor),
+            s.leftAnchor.constraint(equalTo: si.leftAnchor),
+            s.rightAnchor.constraint(equalTo: si.rightAnchor),
+            s.bottomAnchor.constraint(equalTo: si.bottomAnchor)
+            ])
+    }
+    
+    func buildStackView() {
+        let (s, refs) = stackView(initial: State(), renderers: form(), message: {
+            print($0)
         })
         self.refs = refs
         
@@ -50,7 +84,7 @@ class ViewController: UIViewController {
             s.topAnchor.constraint(equalTo: si.topAnchor),
             s.leftAnchor.constraint(equalTo: si.leftAnchor),
             s.rightAnchor.constraint(equalTo: si.rightAnchor)
-        ])
+            ])
     }
 
     override func didReceiveMemoryWarning() {
